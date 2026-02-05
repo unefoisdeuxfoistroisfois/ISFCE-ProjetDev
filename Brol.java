bc6bf0c42a3997271326ffeda9861509ceeb201f
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-02-05 13:16:52 +0100

    Début de code

--- Brol.java
+++ Brol.java
@@ -4,32 +4,18 @@ import lombok.Data;
 public class Brol {
 
   private String nom; //c'est celui qui va recevoir nom (String nom)
-  private boolean ok;
+  private boolean ok = true;
 
-  public Brol(String nom) {
-    //this va etre utilisé dans tout notre class
+  public Brol(String nom){
     this.nom = nom;
-    this.ok = true;
-  }
-
-  public boolean equals(Object objrecu){
-
-    // 1. Si c'est le même objet en mémoire
-    if (this == objrecu) return true;
-
-    // 2. Si obj est null ou pas de la même classe
-    if (objrecu == null || getClass() != objrecu.getClass()) return false;
-
-    // 3. Cast en Brol
-    Brol other = (Brol) objrecu;
-
-    // 4. Comparer les attributs importants
-    return nom.equals(other.nom);
   }
 
   public static void main(String[] args) {
-    Brol objet1 = new Brol("Bradley");
-    Brol objet2 = new Brol("Bradley");
+    Brol objet1;
+    Brol objet2;
+
+    objet1 = new Brol("Bradley");
+    objet2 = new Brol("Bradley");
 
     System.out.println(objet1);
     System.out.println(objet2);
